d7aa06dc04116dd95076de9b21cfbc402ef58a73
Oren Cohen <orencodes@gmail.com>
2019-08-01 21:49:57 +0300

    setting server.js - initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+// Api Routes
+
+
+const app = express();
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+.connect(db, {useNewUrlParser: true, useFindAndModify: false})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+const port = process.env.PORT || 4000;
+
+// Use Routes
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
