e61391d4d029c54405ec28cae49ae35e30050430
Oren Cohen <orencodes@gmail.com>
2019-10-07 18:22:57 +0300

    version 1.0.1

--- client/src/app/services/category.service.ts
+++ client/src/app/services/category.service.ts
@@ -17,6 +17,6 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
 
   getAllCategories(): Observable<Category[]>{
-    return this.http.get<Category[]>('api/category/categories');
+    return this.http.get<Category[]>('http://localhost:4000/api/category/categories');
   }
 }
