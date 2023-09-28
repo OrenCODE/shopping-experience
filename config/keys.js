0421dd4fa2be176d35f4c6991f818c0a7b05cbd5
Oren Cohen <orencodes@gmail.com>
2023-09-28 10:05:29 +0300

    update project version

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports =  {
-    mongoURI : 'mongodb+srv://oren:zZaxntzzpxTlejxb@meanauthapp.pcwdl.mongodb.net/?retryWrites=true&w=majority',
+    mongoURI : 'mongodb+srv://oren:zZaxntzzpxTlejxb@meanauthapp.pcwdl.mongodb.net/shopping?retryWrites=true&w=majority',
     secretOrKey:'secret',
 }
